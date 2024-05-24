4620081e50960bc5f27fa2bb3eb8715f17d2471c
JuyeonChae <jchae1126@gmail.com>
2024-05-24 17:40:21 +0900

    it does something

--- wordpoppie.py
+++ wordpoppie.py
@@ -5,8 +5,20 @@ from dotenv import load_dotenv
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import re
+import deepl
 
 load_dotenv()
+
+def translate(data): 
+    deepl_auth_key = os.getenv('DEEPL_API_KEY')
+    translator = deepl.Translator(deepl_auth_key)
+    
+    for string in data:
+        # Split the string into words
+        words = string.split()        
+        words_string = ' '.join(words)
+        result = translator.translate_text(words_string, target_lang="KO")
+        print(result.text)
 
 # Path to the credentials JSON file downloaded from Google Cloud Console
 credentials_path = os.getenv('GOOGLE_SHEETS_CREDENTIALS')
@@ -21,7 +33,6 @@ SCOPES = [
 # Load credentials from the JSON file
 credentials = service_account.Credentials.from_service_account_file(credentials_path, scopes=SCOPES)
 gc = gspread.authorize(credentials)
-# gc.share('chae.alexa@gmail.com', perm_type='user', role='owner')
 
 # Create a service object for interacting with Google Sheets API
 service = build('sheets', 'v4', credentials=credentials)
@@ -39,7 +50,7 @@ worksheet = doc.worksheet('3')
 
 # take data from a column and separate the cells one by one, remaining alphabets only
 column_data = worksheet.col_values(1)
-pattern = re.compile(r'^[a-zA-Z\s]+$')
-filtered_strings = [cell for cell in column_data if isinstance(cell, str) and pattern.match(cell)]
+print(column_data)
 
-print(filtered_strings)
+translate(column_data)
+    
